dd2d3fa1e7356dd07594d01a5872f8366b7ed596
MalesJanneth <taly.males1@gmail.com>
2026-06-12 16:26:08 -0500

    Tarea Semana 3

--- programacion_poo/main.py
+++ programacion_poo/main.py
@@ -1,8 +1,9 @@
+#Importar la clase Mascota
 from mascota import Mascota
-
+#Crear objetos
 mascota1 = Mascota ("Duque", "Golden", 1)
 mascota2 = Mascota ("Grisly", "León", 1)
-
+#Mostrar información y ejecutar métodos
 mascota1.mostrar_informacion()
 mascota1.hacer_sonido()
 
--- programacion_poo/mascota.py
+++ programacion_poo/mascota.py
@@ -1,13 +1,15 @@
+#Clase mascota
 class Mascota:
+    #Constructor
     def __init__(self, nombre, especie, edad):
         self.nombre = nombre
         self.especie = especie
         self.edad = edad
-
+    #Metodo para mostrar información
     def mostrar_informacion(self):
         print("Nombre: ", self.nombre)
         print("Especie: ", self.especie)
         print("Edad: ", self.edad)
-
+    #Método para realizar una accion
     def hacer_sonido(self):
         print("La mascota esta haciendo un sonido")
